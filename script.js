6e5853721868bfb37462d789ff4bb1961ecf601e
Gabriele Canepa <gabriele@canepa.io>
2018-03-05 20:52:20 +0000

    Update page with iconset and Tippy.js library

---
+++ script.js
@@ -0,0 +1,16 @@
+tippy('#mailIcon', {
+  html: document.querySelector('#mailTooltip'),
+  placement: 'right',
+  arrow: true,
+  interactive: true,
+  distance: 20
+});
+
+// tippy('#mailTooltip', {
+//   html: document.querySelector('#myTemplate'), // Direct element option
+//   arrow: true,
+//   animation: 'fade',
+//   distance: 15,
+//   arrowTransform: 'scale(2)'
+// })
+
