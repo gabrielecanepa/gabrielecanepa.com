72414a362a14e75c5609eaddccd19b0db9a498de
Gabriele Canepa <gabriele@canepa.io>
2018-03-06 13:17:23 +0000

    Add avatar tooltip and create .gitignore file

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-tippy('#mailIcon', {
+// Avatar tooltip
+const message = document.querySelector('#randomMessage');
+const initialText = message.textContent;
+const avatarTip = tippy('#avatarTooltip', {
+  arrow: true,
+  distance: 20,
+  interactive: true,
+  html: '#randomMessage',
+  onShow() {
+    const quotes = ['Hello', 'Hola']
+    // `this` inside callbacks refers to the popper element
+    const content = this.querySelector('.tippy-content')
+    content.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
+    avatarTip.loading = false;
+  },
+  onHidden() {
+    const content = this.querySelector('.tippy-content')
+    content.innerHTML = initialText
+  },
+  // prevent tooltip from displaying over button
+  popperOptions: {
+    modifiers: {
+      preventOverflow: {
+        enabled: false
+      },
+      hide: {
+        enabled: false
+      }
+    }
+  }
+});
+
+// Mail tooltip
+const mailTip = tippy('#mailIcon', {
   html: document.querySelector('#mailTooltip'),
   placement: 'right',
   arrow: true,
   interactive: true,
   distance: 20
 });
-
